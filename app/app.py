75c5ef8c743575f9a1562b57d113f064c405c42a
Tanmay337442 <tanmay337442@gmail.com>
2024-08-13 17:05:22 +0530

    corrected url

--- app/app.py
+++ app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for, make_response
 from flask_socketio import SocketIO, join_room, leave_room, emit
-from flask_cors import CORS
 import logic
 import uuid
 
 app = Flask(__name__)
-CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True, async_handlers=True)
 app.secret_key = '06ae106f5b4e740059c97782'
 client_rooms = {}
@@ -126,5 +124,5 @@ def handle_disconnect():
 def handle_message(message):
     emit('message', message, broadcast=True)
 
-# if __name__ == '__main__':
-#     socketio.run(app)
+if __name__ == '__main__':
+    socketio.run(app)
